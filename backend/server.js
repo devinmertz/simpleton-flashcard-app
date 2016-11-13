cc2f72e0c9ed2592d3ba3cd6dd52e498858ba335
Tanner Plauché <tannerplauche@Tanners-MacBook-Air.local>
2016-11-12 21:55:22 -0700

    Update Readme.md, update server.

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,14 @@ var port = process.env.PORT || 9000;
 
 var config = require('./config');
 var authRoutes = require('./routes/authRoutes');
-var wordlistRoutes = require('./routes/wo');
+var wordlistRoutes = require('./routes/wordListRoutes');
 
-mongoose.connect(config.database);
+// mongoose.connect(config.database);
 
+app.use(express.static('../frontend/app'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
 
 app.use('/api', expressJwt({secret: config.secret}));
 app.use('/api/wordlist/', wordlistRoutes);
